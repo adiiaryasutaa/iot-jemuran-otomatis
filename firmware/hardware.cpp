3518f98b8ca6380b6c7e44694b390c560538bfe7
Adi Aryasuta <adiaryasuta.dev@gmail.com>
2026-06-16 13:58:49 +0800

    feat(firmware): non-blocking LED confirm blink in updateLed()

--- firmware/hardware.cpp
+++ firmware/hardware.cpp
@@ -10,6 +10,22 @@ void applyState(bool isRaining) {
 }
 
 void updateLed() {
+  if (confirmBlinksRemaining > 0) {
+    if (millis() >= confirmBlinkDeadlineMs) {
+      if (!confirmBlinkPhase) {
+        digitalWrite(Pin::LED, HIGH);
+        confirmBlinkPhase      = true;
+        confirmBlinkDeadlineMs = millis() + 150;
+      } else {
+        digitalWrite(Pin::LED, LOW);
+        confirmBlinkPhase      = false;
+        confirmBlinkDeadlineMs = millis() + 150;
+        confirmBlinksRemaining--;
+      }
+    }
+    return;
+  }
+
   if (!confirmedRaining) {
     digitalWrite(Pin::LED, LOW);
     ledState = false;
